f53e84b562f5a945bd2e70712e505fb9ea8f6f2c
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-08 09:04:47 +0100

    feat: DbContext init

---
+++ FilmForge.Entities/Context/FilmForgeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FilmForge.Entities.EntityModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmForge.Entities.Context;
+
+internal class FilmForgeDbContext : DbContext
+{
+    public FilmForgeDbContext(DbContextOptions<FilmForgeDbContext> options)
+        : base(options) { }
+
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+}
