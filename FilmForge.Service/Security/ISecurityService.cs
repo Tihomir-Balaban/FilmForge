7372d8ab6ae0691ba0de15d7f782bd4c6b47d47f
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-10 18:02:12 +0100

    feat: Security service interface

---
+++ FilmForge.Service/Security/ISecurityService.cs
@@ -0,0 +1,11 @@
+﻿
+using FilmForge.Models.Dto;
+
+namespace Service.Security;
+
+public interface ISecurityService
+{
+    (byte[] PasswordHash, byte[] Salt) HashPassword(string password);
+    bool VerifyPassword(string password, byte[] storedHash, byte[] storedSalt);
+    string JwtSecurityHandler(UserDto user);
+}
