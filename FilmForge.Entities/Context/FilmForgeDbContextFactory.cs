2ec4add410a3acc771470c2f405f7bab92cce959
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-10 17:37:30 +0100

    feat: DB context factory implementation

---
+++ FilmForge.Entities/Context/FilmForgeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FilmForge.Entities.Context;
+
+public class FilmForgeDbContextFactory : IDesignTimeDbContextFactory<FilmForgeDbContext>
+{
+    public FilmForgeDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "../FilmForge/appsettings.json"), optional: false)
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<FilmForgeDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseSqlServer(connectionString);
+
+        return new FilmForgeDbContext(builder.Options);
+    }
+}
