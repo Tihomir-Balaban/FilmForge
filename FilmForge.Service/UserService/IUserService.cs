3d3392c9fdde1434341cc352840750131337883a
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-11 18:16:46 +0100

    feat: IUserService created

---
+++ FilmForge.Service/UserService/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using FilmForge.Models.Dtos;
+using FilmForge.Repository.Generics;
+
+namespace FilmForge.Service.UserService;
+
+public interface IUserService : IGenericService<UserDto>
+{
+    Task<UserDto> GetUserByEmailAndPassord(string email, string password);
+}
