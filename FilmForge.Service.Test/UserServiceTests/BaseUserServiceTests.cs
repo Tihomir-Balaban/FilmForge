4c2093b6ffa8e83009a0162ae01ef0184dc1d55e
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-11 18:18:15 +0100

    feat: User Service tests implemented

---
+++ FilmForge.Service.Test/UserServiceTests/BaseUserServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Bogus;
+using FilmForge.Common.Enum;
+using FilmForge.Entities.EntityModels;
+using FilmForge.Models.Dtos;
+using FilmForge.Repository.UserRepository;
+using US = FilmForge.Service.UserService;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmForge.Service.Test.UserServiceTests;
+
+public class BaseUserServiceTests
+{
+    protected readonly Mock<IUserRepository> UserRepositoryMock = new();
+    protected readonly Mock<ILogger<US.UserService>> LoggerMock = new();
+    protected readonly Mock<IMapper> MapperMock = new();
+
+    protected US.UserService UserService;
+
+    private protected void InitService()
+    {
+        UserService = new US.UserService(UserRepositoryMock.Object, LoggerMock.Object, MapperMock.Object);
+    }
+
+
+    private protected UserDto[] ArrangeUserDtos(int amount)
+    {
+        return new Faker<UserDto>()
+            .RuleFor(x => x.Name, y => y.Person.FullName)
+            .RuleFor(x => x.Email, y => y.Internet.Email())
+            .RuleFor(x => x.Role, y => y.PickRandom<UserRole>())
+            .RuleFor(x => x.Password, y => y.Internet.Password())
+            .RuleFor(x => x.Salt, y => string.Empty)
+            .RuleFor(x => x.CreatedOn, y => y.Date.Past(1))
+            .RuleFor(x => x.ModifiedOn, (y, x) => y.Date.Between(x.CreatedOn, DateTime.UtcNow))
+            .Generate(amount)
+            .ToArray();
+    }
+
+    private protected User[] ArrangeUserEntities(int amount)
+    {
+        return new Faker<User>()
+            .RuleFor(x => x.Name, y => y.Person.FullName)
+            .RuleFor(x => x.Email, y => y.Internet.Email())
+            .RuleFor(x => x.Role, y => y.PickRandom<UserRole>())
+            .RuleFor(x => x.Password, y => y.Random.Bytes(42))
+            .RuleFor(x => x.Salt, y => y.Random.Bytes(42))
+            .RuleFor(x => x.CreatedOn, y => y.Date.Past(1))
+            .RuleFor(x => x.ModifiedOn, (y, x) => y.Date.Between(x.CreatedOn, DateTime.UtcNow))
+            .Generate(amount)
+            .ToArray();
+    }
+}
---
+++ FilmForge.Service.Test/UserServiceTests/UserServiceTests.cs
@@ -0,0 +1,153 @@
+﻿using AutoMapper;
+using FilmForge.Entities.EntityModels;
+using FilmForge.Models.Dtos;
+using FilmForge.Repository.UserRepository;
+using FilmForge.Service.UserService;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace FilmForge.Service.Test.UserServiceTests;
+
+public class UserServiceTests : BaseUserServiceTests
+{
+
+    [Fact]
+    public async Task CreateAsync_ShouldReturnUserDto_WhenUserIsCreated()
+    {
+        // Arrange
+        var userDto = ArrangeUserDtos(1).FirstOrDefault();
+        var user = ArrangeUserEntities(1).FirstOrDefault();
+
+        UserRepositoryMock
+            .Setup(r => r.CreateAsync(user, userDto))
+            .ReturnsAsync(userDto);
+
+        MapperMock
+            .Setup(m => m.Map<User>(It.IsAny<UserDto>()))
+            .Returns(user);
+
+        LoggerMock
+            .Setup(l => l.Log(
+                It.IsAny<LogLevel>(),
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()));
+
+        InitService();
+
+        // Act
+        var result = await UserService.CreateAsync(userDto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<UserDto>(result);
+        UserRepositoryMock.Verify(r => r.CreateAsync(user, userDto), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteByIdAsync_ShouldReturnTrue_WhenUserIsDeleted()
+    {
+        // Arrange
+        int userId = 1;
+        UserRepositoryMock
+            .Setup(r => r.DeleteByIdAsync(userId)).
+            ReturnsAsync(true);
+
+        InitService();
+
+        // Act
+        var result = await UserService.DeleteByIdAsync(userId);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnArrayOfUserDtos()
+    {
+        // Arrange
+        var usersDto = ArrangeUserDtos(10);
+
+        UserRepositoryMock
+            .Setup(r => r.GetAllAsync())
+            .ReturnsAsync(usersDto);
+
+        InitService();
+
+        // Act
+        var result = await UserService.GetAllAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(usersDto.Length, result.Length);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnUserDto_WhenUserExists()
+    {
+        // Arrange
+        int userId = 1;
+        var userDto = ArrangeUserDtos(1).FirstOrDefault();
+
+        UserRepositoryMock
+            .Setup(r => r.GetByIdAsync(userId))
+            .ReturnsAsync(userDto);
+
+        InitService();
+
+        // Act
+        var result = await UserService.GetByIdAsync(userId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<UserDto>(result);
+    }
+
+    [Fact]
+    public async Task GetUserByEmailAndPassword_ShouldReturnUserDto_WhenCredentialsAreValid()
+    {
+        // Arrange
+        var userDto = ArrangeUserDtos(1).FirstOrDefault();
+
+        UserRepositoryMock
+            .Setup(r => r.GetUserByEmailAndPassord(userDto.Email, userDto.Password))
+            .ReturnsAsync(userDto);
+
+        InitService();
+
+        // Act
+        var result = await UserService.GetUserByEmailAndPassord(userDto.Email, userDto.Password);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<UserDto>(result);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ShouldReturnUpdatedUserDto_WhenUserIsUpdated()
+    {
+        // Arrange
+        int userId = 1;
+        var userDto = ArrangeUserDtos(1).FirstOrDefault();
+        var user = ArrangeUserEntities(1).FirstOrDefault();
+
+        MapperMock
+            .Setup(m => m.Map<User>(It.IsAny<UserDto>()))
+            .Returns(user);
+
+        UserRepositoryMock
+            .Setup(r => r.UpdateAsync(userId, user))
+            .ReturnsAsync(userDto);
+
+        InitService();
+
+        // Act
+        var result = await UserService.UpdateAsync(userId, userDto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<UserDto>(result);
+        UserRepositoryMock.Verify(r => r.UpdateAsync(userId, user), Times.Once);
+    }
+}
