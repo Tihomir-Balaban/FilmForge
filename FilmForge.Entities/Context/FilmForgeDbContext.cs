77ea6aa1f09164c797fe16e6c8ac11680e8c26c0
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-11 18:06:17 +0100

    feat: added users to db context

--- FilmForge.Entities/Context/FilmForgeDbContext.cs
+++ FilmForge.Entities/Context/FilmForgeDbContext.cs
@@ -13,4 +13,5 @@ public class FilmForgeDbContext : DbContext
     public DbSet<Actor> Actors { get; set; }
     public DbSet<Movie> Movies { get; set; }
     public DbSet<Genre> Genres { get; set; }
+    public DbSet<User> Users { get; set; }
 }
