8d6a14bfd9a6dc335af92ac50f6bb5c4069995b3
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-11 18:11:44 +0100

    feat: IUserRepository creation

---
+++ FilmForge.Repository/UserRepository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using FilmForge.Entities.EntityModels;
+using FilmForge.Models.Dtos;
+using FilmForge.Repository.Generics;
+
+namespace FilmForge.Repository.UserRepository;
+
+public interface IUserRepository : IGenericRepository<UserDto, User>
+{
+    Task<UserDto> GetUserByEmailAndPassord(string email, string password);
+}
