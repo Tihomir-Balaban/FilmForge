0d50706045ea96f129262882a5bb451ff3afde82
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-12 21:32:14 +0100

    feat: db migration

---
+++ FilmForge.Entities/Migrations/20240312170226_AlterUserTableName.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmForge.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterUserTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_User_UserId",
+                table: "Actors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Directors_User_UserId",
+                table: "Directors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Users_UserId",
+                table: "Actors",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Directors_Users_UserId",
+                table: "Directors",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Users_UserId",
+                table: "Actors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Directors_Users_UserId",
+                table: "Directors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_User_UserId",
+                table: "Actors",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Directors_User_UserId",
+                table: "Directors",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
