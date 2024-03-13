99ea3a1770fe4b790827ecbc5f14629ed848d2b5
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-13 16:27:45 +0100

    Implementing ActorRepository

--- FilmForge.Repository/ActorRepository/ActorRepository.cs
+++ FilmForge.Repository/ActorRepository/ActorRepository.cs
@@ -2,28 +2,167 @@ namespace FilmForge.Repository.ActorRepository;
 
 public class ActorRepository : IActorRepository
 {
-    public Task<ActorDto> CreateAsync(Actor entity, ActorDto dto)
+    private readonly FilmForgeDbContext dbContext;
+    private readonly ILogger<ActorRepository> logger;
+    private readonly IMapper mapper;
+
+    public ActorRepository(
+        FilmForgeDbContext dbContext,
+        ILogger<ActorRepository> logger,
+        IMapper mapper)
     {
-        throw new NotImplementedException();
+        this.dbContext = dbContext;
+        this.logger = logger;
+        this.mapper = mapper;
     }
 
-    public Task<bool> DeleteByIdAsync(int id)
+    public async Task<ActorDto> CreateAsync(Actor actor, ActorDto actorDto)
     {
-        throw new NotImplementedException();
+        logger.LogInformation("Adding a new Actor.");
+        try
+        {
+            await dbContext.AddAsync(actor);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation($"Actor created successfully with Name: {actor.Name}.");
+
+            return mapper.Map<ActorDto>(actor);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Failed to add Actor. Error: {e.Message}.");
+
+            throw new ApplicationException(e.Message);
+        }
     }
 
-    public Task<ActorDto[]> GetAllAsync()
+    public async Task<bool> DeleteByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        logger.LogInformation($"Atempting to delete Actor by id: {id}.");
+
+        try
+        {
+            var actor = await dbContext
+                .Actors
+                .FindAsync(id);
+
+            if (actor == null)
+            {
+                logger.LogWarning($"Actor with ID: {id} not found.");
+
+                return false;
+            }
+
+            dbContext.Actors.Remove(actor);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation($"Actor with id: {id} successfully deleted.");
+
+            return true;
+
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Failed to delete Actor with id: {id}. Error: {e.Message}.");
+
+            throw new ApplicationException(e.Message);
+        }
     }
 
-    public Task<ActorDto> GetByIdAsync(int id)
+    public async Task<ActorDto[]> GetAllAsync()
     {
-        throw new NotImplementedException();
+        logger.LogInformation("Getting all Actors.");
+
+        try
+        {
+            var actors = await dbContext
+                .Actors
+                .ToArrayAsync();
+
+            if (actors == null)
+            {
+                logger.LogWarning("No Actors not found.");
+
+                return null;
+            }
+
+            var actorDtos = mapper.Map<ActorDto[]>(actors);
+
+            logger.LogInformation($"Retrieved all {actorDtos.Count()} Actors.");
+
+            return actorDtos;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Failed to retrieved all Actor. Error: {e.Message}.");
+
+            throw new ApplicationException(e.Message);
+        }
     }
 
-    public Task<ActorDto> UpdateAsync(int id, Actor entity)
+    public async Task<ActorDto> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        logger.LogInformation($"Getting Actor with id: {id}.");
+
+        try
+        {
+            var actor = await dbContext
+                .Actors
+                .FindAsync(id);
+
+            if (actor == null)
+            {
+                logger.LogWarning($"Actor with id: {id} not found.");
+
+                return null;
+            }
+
+            logger.LogInformation($"Actor found successfully with Id: {actor.Id} and Name: {actor.Name}.");
+
+            return mapper.Map<ActorDto>(actor);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Failed to find Actor with id {id}. Error: {e.Message}.");
+
+            throw new ApplicationException(e.Message);
+        }
+    }
+
+    public async Task<ActorDto> UpdateAsync(int id, Actor actor)
+    {
+        logger.LogInformation($"Updating Actor: {actor.Name}.");
+
+        try
+        {
+            var actorEntity = await dbContext
+                .Actors
+                .FindAsync(actor.Id);
+
+            if (actor == null)
+            {
+                logger.LogWarning($"Actor with id: {id} not found.");
+
+                return null;
+            }
+
+            actor.CreatedOn = actorEntity.CreatedOn;
+            actor.ModifiedOn = DateTime.Now;
+
+            actorEntity = mapper.Map<Actor>(actor);
+
+            dbContext.Entry(actorEntity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation($"Actor updated successfully with Name: {actor.Name}.");
+
+            return mapper.Map<ActorDto>(actorEntity);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Failed to update Actor. Error: {e.Message}.");
+
+            throw new ApplicationException(e.Message);
+        }
     }
 }
