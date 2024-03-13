89da6774e6d7a86075ca4743934866c82e22f548
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-13 09:34:15 +0100

    feat: made a seed for movie genres

--- FilmForge.Entities/Initializer/DbInitializer.cs
+++ FilmForge.Entities/Initializer/DbInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using FilmForge.Common.Enum;
 using FilmForge.Entities.Context;
 using FilmForge.Entities.EntityModels;
+using System;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -14,29 +15,87 @@ public static class DbInitializer
 
         if (!dbContext.Users.Any())
         {
-            var user = GenerateInitSuperAdminUser();
+            var users = GenerateInitSuperAdminUser();
 
-            dbContext.Users.Add(user);
+            dbContext.Users.AddRange(users);
+            dbContext.SaveChanges();
+        }
+        else if (!dbContext.Genres.Any())
+        {
+            var genres = GenerateInitGenre();
+
+            dbContext.Genres.AddRange(genres);
             dbContext.SaveChanges();
         }
         else
             return;
     }
 
-    private static User GenerateInitSuperAdminUser()
+    private static User[] GenerateInitSuperAdminUser()
     {
         using (var hmac = new HMACSHA512())
         {
-            return new User
+            return new[]
             {
-                Name = "Tihomir Balaban",
-                Email = "tihomir@balaban.com",
-                Role = UserRole.SuperAdministrator,
-                Salt = hmac.Key,
-                Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")),
-                CreatedOn = DateTime.Now,
-                ModifiedOn = DateTime.Now
-            };
+                new User
+                {
+                    Name = "Tihomir Balaban",
+                    Email = "tihomir@balaban.com",
+                    Role = UserRole.SuperAdministrator,
+                    Salt = hmac.Key,
+                    Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")),
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = DateTime.Now
+                },
+                new User
+                {
+                    Name = "Director Directorson",
+                    Email = "director@role.com",
+                    Role = UserRole.Director,
+                    Salt = hmac.Key,
+                    Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")),
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = DateTime.Now
+                },
+                new User
+                {
+                    Name = "Actor Actorson",
+                    Email = "actor@role.com",
+                    Role = UserRole.Actor,
+                    Salt = hmac.Key,
+                    Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")),
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = DateTime.Now
+                }
+            };             
         }
+    }
+
+
+    private static Genre[] GenerateInitGenre()
+    {
+        return new[]
+        {
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Action" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Adventure" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Comedy" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Drama" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Fantasy" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Historical" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Horror" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Science Fiction (Sci-Fi)" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Musical/Dance" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Mystery" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Romance" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Thriller/Suspense" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Western" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Documentary" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Biographical (Biopic)" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Animation" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Family" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "War" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Crime" },
+            new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Film Noir" },
+        };
     }
 }
