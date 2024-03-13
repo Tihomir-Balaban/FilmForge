746d6f16c5b38861594b87ebd9f2c770ba3c216f
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-13 16:23:35 +0100

    feat: extending the Data Seeder

--- FilmForge.Entities/Initializer/DbInitializer.cs
+++ FilmForge.Entities/Initializer/DbInitializer.cs
@@ -1,7 +1,6 @@
 ﻿using FilmForge.Common.Enum;
 using FilmForge.Entities.Context;
 using FilmForge.Entities.EntityModels;
-using System;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -20,11 +19,58 @@ public static class DbInitializer
             dbContext.Users.AddRange(users);
             dbContext.SaveChanges();
         }
-        else if (!dbContext.Genres.Any())
+
+        if (!dbContext.Genres.Any())
         {
             var genres = GenerateInitGenre();
 
             dbContext.Genres.AddRange(genres);
+            dbContext.SaveChanges();
+        }
+
+        if (!dbContext.Directors.Any())
+        {
+            var users = dbContext
+                .Users
+                .Where(u => u
+                    .Role
+                    .Equals(UserRole.Director))
+                    .ToArray();
+
+            var directors = GenerateInitDirectors();
+
+            foreach (var director in directors)
+            {
+                director.UserId = users
+                    .Where(u => u.Name.Equals(director.Name))
+                    .First()
+                    .Id;
+            }
+
+            dbContext.Directors.AddRange(directors);
+            dbContext.SaveChanges();
+        }
+
+        if (!dbContext.Actors.Any())
+        {
+            var users = dbContext
+                .Users
+                .Where(u => u
+                    .Role
+                    .Equals(UserRole.Actor))
+                    .ToArray();
+
+            var actors = GenerateInitActors();
+
+            foreach (var actor in actors)
+            {
+                actor.UserId = users
+                    .Where(u => u.Name.Equals(actor.Name))
+                    .First()
+                    .Id;
+            }
+
+            dbContext.Actors.AddRange(actors);
             dbContext.SaveChanges();
         }
         else
@@ -47,30 +93,24 @@ public static class DbInitializer
                     CreatedOn = DateTime.Now,
                     ModifiedOn = DateTime.Now
                 },
-                new User
-                {
-                    Name = "Director Directorson",
-                    Email = "director@role.com",
-                    Role = UserRole.Director,
-                    Salt = hmac.Key,
-                    Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")),
-                    CreatedOn = DateTime.Now,
-                    ModifiedOn = DateTime.Now
-                },
-                new User
-                {
-                    Name = "Actor Actorson",
-                    Email = "actor@role.com",
-                    Role = UserRole.Actor,
-                    Salt = hmac.Key,
-                    Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")),
-                    CreatedOn = DateTime.Now,
-                    ModifiedOn = DateTime.Now
-                }
+                new User { Name = "Nhristopher Colan", Email = "Nhristopher.Colan@role.com", Role = UserRole.Director, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Tuentin Qarantino", Email = "Tuentin.Qarantino@role.com", Role = UserRole.Director, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Bathryn Kigelow", Email = "Bathryn.Kigelow@role.com", Role = UserRole.Director, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Ruy Gitchie", Email = "Ruy.Gitchie@role.com", Role = UserRole.Director, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Bichael May", Email = "Bichael.May@role.com", Role = UserRole.Director, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Jwayne Dohnson", Email = "Jwayne.Dohnson@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Dobert Rowney", Email = "Dobert.Rowney@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Deonardo LiCaprio", Email = "Lleonardo.Dicaprio@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Hhris Cemsworth", Email = "Hemsworth.Chris@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Hom Tiddleston", Email = "Hiddleston.Tom@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Patalie Nortman", Email = "Patalie.Nortman@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Lennifer Jawrence", Email = "Lennifer.Jawrence@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Raoirse Sonan", Email = "Raoirse.Sonan@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Rargot Mobbie", Email = "Rargot.Mobbie@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now },
+                new User { Name = "Kicole Nidman", Email = "Kicole.Nidman@role.com", Role = UserRole.Actor, Salt = hmac.Key, Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("pw123pw")), CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now }
             };             
         }
     }
-
 
     private static Genre[] GenerateInitGenre()
     {
@@ -96,6 +136,35 @@ public static class DbInitializer
             new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "War" },
             new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Crime" },
             new Genre(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Film Noir" },
+        };
+    }
+
+    private static Actor[] GenerateInitActors()
+    {
+        return new[]
+        {
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Jwayne Dohnson", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Dobert Rowney", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Deonardo LiCaprio", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Hhris Cemsworth", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Hom Tiddleston", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Patalie Nortman", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Lennifer Jawrence", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Raoirse Sonan", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Rargot Mobbie", Bio = "", UserId = 0 },
+            new Actor(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Kicole Nidman", Bio = "", UserId = 0 },
+        };
+    }
+
+    private static Director[] GenerateInitDirectors()
+    {
+        return new[]
+        {
+            new Director(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Nhristopher Colan", Bio = "", UserId = 0 },
+            new Director(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Tuentin Qarantino", Bio = "", UserId = 0 },
+            new Director(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Bathryn Kigelow", Bio = "", UserId = 0 },
+            new Director(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Ruy Gitchie", Bio = "", UserId = 0 },
+            new Director(){ CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, Name = "Bichael May", Bio = "", UserId = 0 },
         };
     }
 }
