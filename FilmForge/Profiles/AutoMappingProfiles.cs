1453c517fe26afa936a375d5abea5206ab241f52
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-13 13:10:36 +0100

    Update AutoMappingProfiles.cs

--- FilmForge/Profiles/AutoMappingProfiles.cs
+++ FilmForge/Profiles/AutoMappingProfiles.cs
@@ -1,54 +1,53 @@
 ﻿using FilmForge.Entities.EntityModels;
 using System.Text;
 
-namespace FilmForge.Models.Profiles
+namespace FilmForge.Models.Profiles;
+
+public class AutoMappingProfiles : Profile
 {
-    public class AutoMappingProfiles : Profile
+    public AutoMappingProfiles()
     {
-        public AutoMappingProfiles()
-        {
-            CreateReverseMaps();
-            CreateDto2EntityMaps();
-            CreateEntity2DtoMaps();
-        }
+        CreateReverseMaps();
+        CreateDto2EntityMaps();
+        CreateEntity2DtoMaps();
+    }
 
-        private void CreateReverseMaps()
-        {
-            CreateMap<ActorDto, Actor>()
-               .ReverseMap();
+    private void CreateReverseMaps()
+    {
+        CreateMap<ActorDto, Actor>()
+           .ReverseMap();
 
-            CreateMap<DirectorDto, Director>()
-            .ReverseMap();
+        CreateMap<DirectorDto, Director>()
+        .ReverseMap();
 
-            CreateMap<MovieDto, Movie>()
-               .ReverseMap();
+        CreateMap<MovieDto, Movie>()
+           .ReverseMap();
 
-            CreateMap<GenreDto, Genre>()
-               .ReverseMap();
+        CreateMap<GenreDto, Genre>()
+           .ReverseMap();
 
-            CreateMap<RatingDto, Rating>()
-               .ReverseMap();
+        CreateMap<RatingDto, Rating>()
+           .ReverseMap();
 
-            CreateMap<ReviewDto, Review>()
-               .ReverseMap();
-        }
+        CreateMap<ReviewDto, Review>()
+           .ReverseMap();
+    }
 
-        private void CreateDto2EntityMaps()
-        {
-            CreateMap<UserDto, User>()
-                .ForMember(des => des.Password,
-                           opt => opt.MapFrom(src => Encoding.UTF8.GetBytes(src.Password)))
-                .ForMember(des => des.Salt,
-                           opt => opt.MapFrom(src => Encoding.UTF8.GetBytes(src.Salt)));
-        }
+    private void CreateDto2EntityMaps()
+    {
+        CreateMap<UserDto, User>()
+            .ForMember(des => des.Password,
+                       opt => opt.MapFrom(src => Encoding.UTF8.GetBytes(src.Password)))
+            .ForMember(des => des.Salt,
+                       opt => opt.MapFrom(src => Encoding.UTF8.GetBytes(src.Salt)));
+    }
 
-        private void CreateEntity2DtoMaps()
-        {
-            CreateMap<User, UserDto>()
-                .ForMember(des => des.Password,
-                           opt => opt.MapFrom(src => string.Empty))
-                .ForMember(des => des.Salt,
-                           opt => opt.MapFrom(src => string.Empty));
-        }
+    private void CreateEntity2DtoMaps()
+    {
+        CreateMap<User, UserDto>()
+            .ForMember(des => des.Password,
+                       opt => opt.MapFrom(src => string.Empty))
+            .ForMember(des => des.Salt,
+                       opt => opt.MapFrom(src => string.Empty));
     }
 }
