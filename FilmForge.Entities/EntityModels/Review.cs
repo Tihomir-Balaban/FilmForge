59ce54f20990366f47af400876babb891dadb876
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-13 10:13:07 +0100

    feat: Added Review and Rating Entities

---
+++ FilmForge.Entities/EntityModels/Review.cs
@@ -0,0 +1,17 @@
+﻿using FilmForge.Entities.EntityModels.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmForge.Entities.EntityModels;
+
+public class Review : BaseEntity
+{
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public int MovieId { get; set; }
+    public Movie Movie { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
