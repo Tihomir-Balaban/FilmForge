4b36b7e9b79cbe90e42b3d8cba5b56252f2a13f0
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-13 10:13:30 +0100

    feat: Adding new entities to db context

--- FilmForge.Entities/Context/FilmForgeDbContext.cs
+++ FilmForge.Entities/Context/FilmForgeDbContext.cs
@@ -12,4 +12,6 @@ public class FilmForgeDbContext : DbContext
     public DbSet<Movie> Movies { get; set; }
     public DbSet<Genre> Genres { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<Review> Reviews { get; set; }
 }
