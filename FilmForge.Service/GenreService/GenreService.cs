e3bcb5b7f688468df65b35ba79e80cdeceb5183f
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-13 17:24:55 +0100

    feat: GenreService Implemented

--- FilmForge.Service/GenreService/GenreService.cs
+++ FilmForge.Service/GenreService/GenreService.cs
@@ -1,29 +1,67 @@
+using FilmForge.Repository.GenreRepository;
+
 namespace FilmForge.Service.GenreService;
 
 public class GenreService : IGenreService
 {
-    public Task<GenreDto> CreateAsync(GenreDto dto)
+    private readonly IGenreRepository genreRepository;
+    private readonly ILogger<GenreService> logger;
+    private readonly IMapper mapper;
+
+    public GenreService(
+        IGenreRepository genreRepository,
+        ILogger<GenreService> logger,
+        IMapper mapper)
     {
-        throw new NotImplementedException();
+        this.genreRepository = genreRepository;
+        this.logger = logger;
+        this.mapper = mapper;
     }
 
-    public Task<bool> DeleteByIdAsync(int id)
+    public async Task<GenreDto> CreateAsync(GenreDto genreDto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            (genreDto.CreatedOn, genreDto.ModifiedOn) = (DateTime.Now, DateTime.Now);
+
+            logger.LogInformation($"Mapping GenreDto to Genre (Entity) in GenreService CreateAsync");
+
+            var genre = mapper.Map<Genre>(genreDto);
+
+            return await genreRepository.CreateAsync(genre, genreDto);
+        }
+        catch (Exception e)
+        {
+
+            logger.LogError(e, $"Failed to map Genre. Error: {e.Message}.");
+
+            throw new ApplicationException(e.Message);
+        }
     }
 
-    public Task<GenreDto[]> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<bool> DeleteByIdAsync(int id)
+        => await genreRepository.DeleteByIdAsync(id);
 
-    public Task<GenreDto> GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<GenreDto[]> GetAllAsync()
+        => await genreRepository.GetAllAsync();
 
-    public Task<GenreDto> UpdateAsync(int id, GenreDto dto)
+    public async Task<GenreDto> GetByIdAsync(int id)
+        => await genreRepository.GetByIdAsync(id);
+
+    public async Task<GenreDto> UpdateAsync(int id, GenreDto genreDto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            logger.LogInformation($"Mapping GenreDto to Genre (Entity) in GenreService UpdateAsync");
+            var genre = mapper.Map<Genre>(genreDto);
+
+            return await genreRepository.UpdateAsync(id, genre);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Failed to map Genre. Error: {e.Message}.");
+
+            throw new ApplicationException(e.Message);
+        }
     }
 }
