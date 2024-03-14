48e79bbbbb249172aed10075d7b529045e68cafb
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-03-14 09:22:38 +0100

    Create 20240314071852_AlterMovieTableAddingFields.cs

---
+++ FilmForge.Entities/Migrations/20240314071852_AlterMovieTableAddingFields.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmForge.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterMovieTableAddingFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Budget",
+                table: "Movies",
+                type: "decimal(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Budget",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Movies");
+        }
+    }
+}
